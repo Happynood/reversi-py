c0f45dba8531c7bed0e6cdd1a5a0eb03ee148633
Happynood <cthjd24@yandex.ru>
2023-11-04 18:04:43 +0300

    add-main-and-imports-with-settings

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+import random
+import copy
+
+# Define constants
+BOARD_SIZE = 8
+SQUARE_SIZE = 50
+WINDOW_SIZE = (BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 200, 0)
+BLUE = (0, 0, 200)
+RED = (200, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Reversi")
