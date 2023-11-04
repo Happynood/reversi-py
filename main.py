b18fb27b6dc2338934602af6b1ec15a96d4c57ed
Happynood <cthjd24@yandex.ru>
2023-11-04 18:12:51 +0300

    add-move

--- main.py
+++ main.py
@@ -29,3 +29,43 @@ def draw_board(board):
                 pygame.draw.circle(screen, BLACK, rect.center, SQUARE_SIZE // 2 - 5)
             elif board[x][y] == 2:
                 pygame.draw.circle(screen, WHITE, rect.center, SQUARE_SIZE // 2 - 5)
+
+def get_valid_moves(board, player):
+    valid_moves = []
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+            if board[x][y] == 0:
+                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]:
+                    nx, ny = x + dx, y + dy
+                    if nx < 0 or nx >= BOARD_SIZE or ny < 0 or ny >= BOARD_SIZE or board[nx][ny] == player:
+                        continue
+                    while board[nx][ny] != 0:
+                        nx += dx
+                        ny += dy
+                        if nx < 0 or nx >= BOARD_SIZE or ny < 0 or ny >= BOARD_SIZE:
+                            break
+                        if board[nx][ny] == player:
+                            valid_moves.append((x, y))
+                            break
+    return valid_moves
+
+def make_move(board, player, move):
+    x, y = move
+    board[x][y] = player
+    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]:
+        nx, ny = x + dx, y + dy
+        if nx < 0 or nx >= BOARD_SIZE or ny < 0 or ny >= BOARD_SIZE or board[nx][ny] == player:
+            continue
+        while board[nx][ny] != 0:
+            nx += dx
+            ny += dy
+            if nx < 0 or nx >= BOARD_SIZE or ny < 0 or ny >= BOARD_SIZE:
+                break
+            if board[nx][ny] == player:
+                while True:
+                    nx -= dx
+                    ny -= dy
+                    if nx == x and ny == y:
+                        break
+                    board[nx][ny] = player
+                break
