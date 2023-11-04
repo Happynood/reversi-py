a09d775a2be54109780610550605fd9064acc18b
Happynood <cthjd24@yandex.ru>
2023-11-04 18:10:16 +0300

    add-draw-board-function

--- main.py
+++ main.py
@@ -18,3 +18,14 @@ YELLOW = (255, 255, 0)
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Reversi")
+
+# Define helper functions
+def draw_board(board):
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+            rect = pygame.Rect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+            pygame.draw.rect(screen, GREEN, rect)
+            if board[x][y] == 1:
+                pygame.draw.circle(screen, BLACK, rect.center, SQUARE_SIZE // 2 - 5)
+            elif board[x][y] == 2:
+                pygame.draw.circle(screen, WHITE, rect.center, SQUARE_SIZE // 2 - 5)
