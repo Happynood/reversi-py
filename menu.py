e329ad10facf3e560699149894e248a9234ff967
Happynood <cthjd24@yandex.ru>
2023-12-03 01:27:08 +0300

    added-menu-file-with-sync

--- menu.py
+++ menu.py
@@ -2,7 +2,10 @@ import pygame
 import pygame_widgets
 from pygame_widgets.button import Button
 from pygame.locals import *
+import sys
 import main
+from pygame_widgets.dropdown import Dropdown
+
 WIDTH = 800
 HEIGHT = 800
 BACKGROUND_COLOR = (0, 0, 0)
@@ -10,41 +13,88 @@ BACKGROUND_COLOR = (0, 0, 0)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Reversi Menu")
-
+screen.blit(pygame.image.load("./img/menu3.png"), (0, 0))
 font = pygame.font.SysFont("Arial", 70)
+btn1 = Button(
+    # Mandatory Parameters
+    screen,  # Surface to place button on
+    WIDTH // 2 - 75,  # X-coordinate of top left corner
+    HEIGHT // 2 - 150,  # Y-coordinate of top left corner
+    150,  # Width
+    100,  # Text to display
+    fontSize=30,  # Size of font
+    margin=20,  # Minimum distance between text/image and edge of button
+    inactiveColour=(144, 77, 48),  # Colour of button when not being interacted with
+    text='Start Game',
+    pressedColour=(140, 71, 67),  # Colour of button when being clicked
+    radius=20,  # Radius of border corners (leave empty for not curved)
+    onClick=lambda: main.main()  # Function to call when clicked on
+)
 
-button = Button(
-            # Mandatory Parameters
-            screen,  # Surface to place button on
-            WIDTH //2 -75,  # X-coordinate of top left corner
-            HEIGHT//2-75,  # Y-coordinate of top left corner
-            150,  # Width
-            100,  # Text to display
-            fontSize=30,  # Size of font
-            margin=20,  # Minimum distance between text/image and edge of button
-            inactiveColour=(255, 255, 255),  # Colour of button when not being interacted with
-            text = 'Start Game',
-            pressedColour=(100, 100, 100),  # Colour of button when being clicked
-            radius=20,  # Radius of border corners (leave empty for not curved)
-            onClick=lambda: main.main()  # Function to call when clicked on
-        )
+btn2= Button(
+    # Mandatory Parameters
+    screen,  # Surface to place button on
+    WIDTH // 2 - 75,  # X-coordinate of top left corner
+    HEIGHT // 2 + 175,  # Y-coordinate of top left corner
+    150,  # Width
+    100,  # Text to display
+    fontSize=30,  # Size of font
+    margin=20,  # Minimum distance between text/image and edge of button
+    inactiveColour=(144, 77, 48),  # Colour of button when not being interacted with
+    text='Quit',
+    pressedColour=(140, 71, 67),  # Colour of button when being clicked
+    radius=20,  # Radius of border corners (leave empty for not curved)
+    onClick=lambda: sys.exit()  # Function to call when clicked on
+)
+
+dropdown1 = Dropdown(
+    screen, WIDTH // 2 -250, HEIGHT // 2 , 150, 100, name='Select mode',
+    choices=[
+        'Reversi',
+        'Antireversi'
+    ],
+    borderRadius=20,
+    colour=pygame.Color((144, 77, 48)),
+    values=[True,False],
+    direction='down',
+    textHAlign='centre',
+    fontSize=30,
+    font = pygame.font.SysFont("Arial", 30)
+)
+
+dropdown2 = Dropdown(
+    screen, WIDTH // 2 +100, HEIGHT // 2 , 150, 100, name='Select color',
+    choices=[
+        'Black',
+        'White'
+    ],
+    borderRadius=20,
+    colour=pygame.Color((144, 77, 48)),
+    values=[True,False],
+    direction='down',
+    textHAlign='centre',
+    fontSize=30,
+    font = pygame.font.SysFont("Arial", 30)
+)
 
 title_text = font.render("Reversi Menu", True, (255, 255, 255))
-screen.blit(title_text, (WIDTH / 2 - title_text.get_width() / 2, HEIGHT / 4))
-def draw_menu():
-    running = True
-    while running:
-        # Handle Events
-        events = pygame.event.get()
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-        # Fill Background
-        pygame_widgets.update(events)  # Call once every loop to allow widgets to render and listen
-        pygame.display.update()
+screen.blit(title_text, (WIDTH / 2 - title_text.get_width() / 2, HEIGHT / 4-100))
 
+
+def draw_menu():
+    run = True
+    while run:
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+                quit()
+
+        screen.fill((0, 0, 0), pygame.Rect(0, 400, 800, 800))
+        screen.blit(pygame.image.load("./img/menu2.png"), (0, 0))
+        pygame_widgets.update(events)
+        pygame.display.update()
 
 draw_menu()
